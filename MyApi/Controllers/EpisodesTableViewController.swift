4750474a2357a5295c69e9d2f18085d08a72d0bb
ddyack <ddyack@yandex.ru>
2020-09-24 15:55:50 +0300

    finish

--- MyApi/Controllers/EpisodesTableViewController.swift
+++ MyApi/Controllers/EpisodesTableViewController.swift
@@ -25,14 +25,9 @@ class EpisodesTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return episodes.count
+        episodes.count
     }
 
 
@@ -40,21 +35,10 @@ class EpisodesTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "episodeCell", for: indexPath)
 
         let episode = episodes[indexPath.row]
-        cell.textLabel?.text = "Название серии \(episode.title ?? "")"
+        cell.textLabel?.text = episode.title
         cell.detailTextLabel?.text = "Дата выхода \(episode.airDate ?? "")"
-        
+    
         return cell
     }
    
-   
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
